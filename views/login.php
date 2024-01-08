9d75615961505de8925b80328dfa7dc6d9aa8f20
Babajii91 <119482681+Babajii91@users.noreply.github.com>
2024-01-08 09:27:20 +0100

    Add files via upload

---
+++ views/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<div id="content">
+    <div class="contact-form">
+        <h2>Connexion</h2>
+        <form action="?page=process_contact" method="post">
+            <label for="identifiant">Identifiant</label>
+            <input type="text" name="nom" id="nom" required>
+
+            <label for="Mot de passe">Mot de passe</label>
+            <input type="text" name="prenom" id="prenom" required>
+
+            <button type="submit">SE CONNECTER</button>
+        </form>
+    </div>
+</div>
+</body>
+</html>
