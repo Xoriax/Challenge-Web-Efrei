ad4b19f6fe1d358c5cac2b61c742e9da036c28a0
Xoriax <152291829+Xoriax@users.noreply.github.com>
2024-01-11 00:02:56 +0100

    Update accueil.php

--- Challenge_Web_Project/vue/accueil.php
+++ Challenge_Web_Project/vue/accueil.php
@@ -1,5 +1,2 @@
-<!-- accueil.php -->
-
 <h1>Accueil</h1>
 <p>Bienvenue sur RestoHub. Découvrez une sélection des meilleurs restaurants.</p>
-<!-- Ajoutez ici le contenu spécifique de la page d'accueil -->
