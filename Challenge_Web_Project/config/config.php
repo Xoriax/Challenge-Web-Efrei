72b65920304c35a1eecec305dc3aaecf259de25b
Xoriax <152291829+Xoriax@users.noreply.github.com>
2024-01-11 00:42:48 +0100

    Update config.php

--- Challenge_Web_Project/config/config.php
+++ Challenge_Web_Project/config/config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'postgres');
-define('DB_USER', 'lcs');
-define('DB_PASSWORD', '123');
-define('DB_PORT', '5432');
+define('DB_USER', 'nom_utilisateur');
+define('DB_PASSWORD', 'mot_de_passe');
+define('DB_PORT', 'port');
 ?>
