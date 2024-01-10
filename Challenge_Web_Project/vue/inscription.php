d3daa7e3deb132384ddea5df529bc81abba57033
Babajii91 <119482681+Babajii91@users.noreply.github.com>
2024-01-10 16:54:01 +0100

    Add files via upload

---
+++ Challenge_Web_Project/vue/inscription.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+<body>
+    <h2>Inscription</h2>
+<form action="?page=process_inscription" method="post">
+    <label for="username">Nom d'utilisateur:</label>
+    <input type="text" id="username" name="username" required>
+
+    <label for="password">Mot de passe:</label>
+    <input type="password" id="password" name="password" required>
+
+    <button type="submit">S'inscrire</button>
+</form>
+</body>
+</html>
