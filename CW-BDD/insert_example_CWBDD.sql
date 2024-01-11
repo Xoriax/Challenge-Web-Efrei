cd34b71b55388f298f75f195f6ce21e497dac0c5
Xoriax <152291829+Xoriax@users.noreply.github.com>
2024-01-11 03:26:56 +0100

    Update insert_example_CWBDD.sql

--- CW-BDD/insert_example_CWBDD.sql
+++ CW-BDD/insert_example_CWBDD.sql
@@ -1,6 +1,17 @@
-INSERT INTO restaurant (name, address, image) VALUES ('Restaurant A', 'Adresse A', 'image_a.jpg');
-INSERT INTO restaurant (name, address, image) VALUES ('Restaurant B', 'Adresse B', 'image_b.jpg');
-INSERT INTO restaurant (name, address, image) VALUES ('Restaurant C', 'Adresse C', 'image_c.jpg');
+INSERT INTO restaurant (name, address, image) VALUES 
+('Restaurant A', 'Adresse A', 'image_a.jpg'),
+('Restaurant B', 'Adresse B', 'image_b.jpg'),
+('Restaurant C', 'Adresse C', 'image_c.jpg'),
+('Restaurant D', 'Adresse D', 'image_d.jpg'),
+('Restaurant E', 'Adresse E', 'image_e.jpg'),
+('Restaurant F', 'Adresse F', 'image_f.jpg'),
+('Restaurant G', 'Adresse G', 'image_g.jpg'),
+('Restaurant H', 'Adresse H', 'image_h.jpg'),
+('Restaurant I', 'Adresse I', 'image_i.jpg'),
+('Restaurant J', 'Adresse J', 'image_j.jpg'),
+('Restaurant K', 'Adresse K', 'image_k.jpg'),
+('Restaurant L', 'Adresse L', 'image_l.jpg'),
+('Restaurant M', 'Adresse M', 'image_m.jpg');
 SELECT * from restaurant
 INSERT INTO plat (name, restaurant_id, image) VALUES ('Plat 1', 1, 'imageplat1');
 INSERT INTO plat (name, restaurant_id, image) VALUES ('Plat 2', 1, 'imageplat2');
