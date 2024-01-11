c07ee05c527d014348be89bf9a07fd51e69d8645
Xoriax <152291829+Xoriax@users.noreply.github.com>
2024-01-11 17:10:07 +0100

    Delete Challenge_Web_Project/vue/user_view.php

--- Challenge_Web_Project/vue/user_view.php
+++
@@ -1,6 +0,0 @@
-<h1>Profil</h1>
-<p>Consultez et mettez à jour votre profil utilisateur.</p>
-<div>
-    <a href="?page=reservation">Mes Réservations</a>
-    <a href="?page=favoris">Mes Favoris</a>
-</div>
