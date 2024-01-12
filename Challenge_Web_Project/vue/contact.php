ad9133d7be8ae838b64008de056d9277f79e37b6
Xoriax <152291829+Xoriax@users.noreply.github.com>
2024-01-12 02:54:28 +0100

    Update contact.php

--- Challenge_Web_Project/vue/contact.php
+++ Challenge_Web_Project/vue/contact.php
@@ -1,2 +1,20 @@
-<h1>Contact</h1>
-<p>Contactez-nous pour toute question ou préoccupation.</p>
+<div id="content">
+    <div class="contact-form">
+        <h2>Contactez-nous</h2>
+        <form class="formcontact" action="?page=process_contact" method="post">
+            <label class="labelcontact" for="nom">Nom :</label>
+            <input class="inputcontact" type="text" name="nom" id="nom" required>
+
+            <label class="labelcontact" for="prenom">PrÃ©nom :</label>
+            <input class="inputcontact" type="text" name="prenom" id="prenom" required>
+
+            <label class="labelcontact" for="email">Email :</label>
+            <input class="inputcontact" type="email" name="email" id="email" required>
+
+            <label class="labelcontact" for="message">Message :</label>
+            <textarea class="areatextcontact" name="message" id="message" rows="4" required></textarea>
+
+            <button class="btncontact" type="submit">Envoyer</button>
+        </form>
+    </div>
+</div>
